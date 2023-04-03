802d47eec405e00733bace0f54c7955c047ccf62
osmantc <blackcalvus06@hotmail.com>
2023-04-03 23:57:33 +0300

    v3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Dating App';
-  users:any;
+  users: any;
 
-constructor() {
-
-
-}
+  constructor(private client: HttpClient) {}
 
   ngOnInit(): void {
-
+    this.client
+      .get('http://localhost:5000/api/user')
+      .subscribe((next) => (this.users = next));
   }
 }
