985ebf5e79196b54ef2029672dd0d4f2d0999618
osmantc <blackcalvus06@hotmail.com>
2023-04-16 23:19:28 +0300

    async pipe eklendi and trash

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { User } from '../_models/user';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -9,30 +10,32 @@ import { User } from '../_models/user';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn: boolean = false;
+  // loggedIn: boolean = false;
 
-  constructor(private accountService: AccountService) {}
+  constructor(public accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    // this.getCurrentUser();
   }
 
   //bu şimdilik mantığı görmek için böyle. bunun yerine bu kadar yazmaya uğraşmadan en iyisi async-pipe kullanımı, daha pratik.
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe({
-      next: (user) => {
-        this.loggedIn = !!user;
-        this.model.username = user?.username;
-      }, // veya this.loggedIn = user !== null
-      error: (error) => console.log(error),
-    });
-  }
+  //asnyc pipe kullanmaya geçtiğimiz için bu logic iptal.
+
+  // getCurrentUser() {
+  //   this.accountService.currentUser$.subscribe({
+  //     next: (user) => {
+  //       this.loggedIn = !!user;
+  //       this.model.username = user?.username;
+  //     }, // veya this.loggedIn = user !== null
+  //     error: (error) => console.log(error),
+  //   });
+  // }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: (response) => {
         console.log(response);
-        this.loggedIn = true;
+        // this.loggedIn = true;
       },
       error: (error) => {
         console.log(error);
@@ -42,6 +45,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
+    // this.loggedIn = false;
   }
 }
